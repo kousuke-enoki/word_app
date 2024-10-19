484fee1bf6a362658ffd8daaffd6e41939167de2
kousuke-enoki <kokoeno129@gmail.com>
2024-10-19 19:33:02 +0900

    05wordlist_単語一覧画面完成、トークン切れでトップへ遷移、ディレクトリ整理

--- frontend/src/pages/user/SignIn.tsx
+++ frontend/src/pages/user/SignIn.tsx
@@ -1,42 +1,30 @@
 import React, { useState } from 'react';
-import axiosInstance from '../axiosConfig';
+import axiosInstance from '../../axiosConfig';
 
-const SignUp: React.FC = () => {
-  const [name, setName] = useState('');
+const SignIn: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
 
-const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
+const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
   e.preventDefault();
   try {
-    const response = await axiosInstance.post('/users/sign_up', {
-        name,
-        email,
-        password,
+    const response = await axiosInstance.post('/users/sign_in', {
+      email,
+      password,
     });
     const token = response.data.token;
     localStorage.setItem('token', token);
-    setMessage('Sign up successful!');
+    setMessage('Sign in successful!');
   } catch (error) {
-    setMessage('Sign up failed. Please try again.');
+    setMessage('Sign in failed. Please try again.');
   }
 };
 
   return (
     <div>
-      <h1>サインアップ</h1>
-      <form onSubmit={handleSignUp}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="name"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
+      <h1>サインイン</h1>
+      <form onSubmit={handleSignIn}>
         <div>
           <label htmlFor="email">Email:</label>
           <input
@@ -57,11 +45,11 @@ const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
             required
           />
         </div>
-        <button type="submit">サインアップ</button>
+        <button type="submit">サインイン</button>
       </form>
       {message && <p>{message}</p>}
     </div>
   );
 };
 
-export default SignUp;
+export default SignIn;

bec3025c5a5d72c830e253988fd2d36add743a68
kousuke-enoki <kokoeno129@gmail.com>
2024-12-23 09:23:57 +0900

    06-01 シェルスクリプト周り修正など

--- execfro.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" = "production" ]; then
-  ENV_FILE="backend/.env.production"
-else
-  ENV_FILE="backend/.env.development"
-fi
-
-docker compose --env-file $ENV_FILE exec frontend bash

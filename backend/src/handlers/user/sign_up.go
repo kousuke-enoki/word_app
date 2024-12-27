718eb110e0ec74e4a11c5a14009d5a34243dfc66
kousuke-enoki <kokoeno129@gmail.com>
2024-12-27 22:06:43 +0900

    merge stash

--- backend/src/handlers/user/sign_up.go
+++ backend/src/handlers/user/sign_up.go
@@ -9,9 +9,11 @@ import (
 	"net/http"
 
 	"word_app/backend/src/models"
+	user_service "word_app/backend/src/service/user"
 	"word_app/backend/src/validators/user"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -29,23 +31,42 @@ func (h *UserHandler) SignUpHandler() gin.HandlerFunc {
 			return
 		}
 
+		logrus.Info("j")
 		validationErrors := user.ValidateSignUp(req)
 		if len(validationErrors) > 0 {
+			logrus.Info("validationErrors")
+			logrus.Info(validationErrors)
 			c.JSON(http.StatusBadRequest, gin.H{"errors": validationErrors})
 			return
 		}
+		logrus.Info("adsf")
 
 		hashedPassword, err := h.hashPassword(req.Password)
 		if err != nil {
+			logrus.Info("err")
+			logrus.Info(err)
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
 			return
 		}
+		logrus.Info("qwer")
 
+		// ユーザー作成
 		user, err := h.userClient.CreateUser(context.Background(), req.Email, req.Name, hashedPassword)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Sign up failed", "details": err.Error()})
+			logrus.Error(err)
+
+			// エラーの種類ごとにレスポンスを変更
+			switch err {
+			case user_service.ErrDuplicateEmail:
+				c.JSON(http.StatusConflict, gin.H{"error": "Email already exists"})
+			case user_service.ErrDatabaseFailure:
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+			default:
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "An unknown error occurred"})
+			}
 			return
 		}
+		logrus.Info("ert")
 
 		token, err := h.jwtGenerator.GenerateJWT(fmt.Sprintf("%d", user.ID))
 		if err != nil {

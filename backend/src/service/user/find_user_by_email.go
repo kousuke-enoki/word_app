214767c9c1a7ed77d417b5bac7ac7a63e959e34e
kousuke-enoki <kokoeno129@gmail.com>
2024-12-29 00:28:04 +0900

    06-02testもろもろ修正など

---
+++ backend/src/service/user/find_user_by_email.go
@@ -0,0 +1,22 @@
+package user_service
+
+import (
+	"context"
+	"word_app/backend/ent"
+	"word_app/backend/ent/user"
+)
+
+func (e *EntUserClient) FindUserByEmail(ctx context.Context, email string) (*ent.User, error) {
+	user, err := e.client.User.
+		Query().
+		Where(user.EmailEQ(email)).
+		First(ctx)
+
+	if err != nil {
+		if ent.IsConstraintError(err) {
+			return nil, ErrDuplicateEmail
+		}
+		return nil, ErrDatabaseFailure
+	}
+	return user, nil
+}

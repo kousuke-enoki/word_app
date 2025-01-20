f53f2d3a82e69d6db4dabe3b99b19f5d0bbde33f
kousuke-enoki <kokoeno129@gmail.com>
2025-01-20 18:00:55 +0900

    06-02 test update, 修正など

--- backend/src/service/word/test/update_word_service_test.go
+++ backend/src/service/word/test/update_word_service_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateWord(t *testing.T) {
+func TestUpdateWord(t *testing.T) {
 	client := enttest.Open(t, "sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
 	defer client.Close()
 
@@ -55,61 +55,126 @@ func TestCreateWord(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, nonAdminUser)
 
+	userID := 1
+	word1 := client.Word.Create().
+		SetName("apple").
+		SetRegistrationCount(5).
+		SaveX(ctx)
+
+	client.Word.Create().
+		SetName("banana").
+		SetRegistrationCount(3).
+		SaveX(ctx)
+
+	client.Word.Create().
+		SetName("orange").
+		SetRegistrationCount(0).
+		SaveX(ctx)
+
+	word2 := client.Word.Create().
+		SetName("approach").
+		SetRegistrationCount(0).
+		SaveX(ctx)
+
+	wordInfo1 := client.WordInfo.Create().
+		SetWordID(word1.ID).
+		SetPartOfSpeechID(1).
+		SaveX(ctx)
+
+	client.JapaneseMean.Create().
+		SetWordInfoID(wordInfo1.ID).
+		SetName("りんご").
+		SaveX(ctx)
+
+	wordInfo2 := client.WordInfo.Create().
+		SetWordID(word2.ID).
+		SetPartOfSpeechID(1).
+		SaveX(ctx)
+	wordInfo3 := client.WordInfo.Create().
+		SetWordID(word2.ID).
+		SetPartOfSpeechID(3).
+		SaveX(ctx)
+
+	client.JapaneseMean.Create().
+		SetWordInfoID(wordInfo2.ID).
+		SetName("方法").
+		SaveX(ctx)
+	client.JapaneseMean.Create().
+		SetWordInfoID(wordInfo3.ID).
+		SetName("近づく").
+		SaveX(ctx)
+
+	client.RegisteredWord.Create().
+		SetWordID(word1.ID).
+		SetUserID(userID).
+		SetIsActive(true).
+		SetAttentionLevel(2).
+		SetTestCount(10).
+		SetCheckCount(5).
+		SetMemo("memo").
+		SaveX(ctx)
+
 	// 正常なリクエストデータ
 	japaneseMean := []models.JapaneseMean{
-		{Name: "テスト"},
+		{ID: 1, Name: "りんご"},
 	}
 	wordInfos := []models.WordInfo{
-		{PartOfSpeechID: 1, JapaneseMeans: japaneseMean}, // 名詞
+		{ID: 1, PartOfSpeechID: 1, JapaneseMeans: japaneseMean}, // 名詞
 	}
-	reqData := &models.CreateWordRequest{
-		Name:      "test",
+	reqData := &models.UpdateWordRequest{
+		ID:        word1.ID,
+		Name:      "apple",
 		WordInfos: wordInfos,
 		UserID:    1, // 存在するユーザーID
 	}
 
 	t.Run("Success", func(t *testing.T) {
 		// テスト実行
-		createdWord, err := wordService.CreateWord(ctx, reqData)
+		createdWord, err := wordService.UpdateWord(ctx, reqData)
 		assert.NoError(t, err)
 		assert.NotNil(t, createdWord)
-		assert.Equal(t, "test", createdWord.Name)
+		assert.Equal(t, 1, createdWord.ID)
+		assert.Equal(t, "apple", createdWord.Name)
+		assert.Equal(t, "word 'apple' updated successfully", createdWord.Message)
 	})
 
 	t.Run("NonAdmin_error", func(t *testing.T) {
 		// 2. 管理者以外のユーザーでリクエスト
-		reqData = &models.CreateWordRequest{
+		reqData = &models.UpdateWordRequest{
+			ID:        word1.ID,
 			Name:      "test",
 			UserID:    nonAdminUser.ID, // 非管理者ユーザー
 			WordInfos: []models.WordInfo{},
 		}
 
-		_, err = wordService.CreateWord(ctx, reqData)
+		_, err = wordService.UpdateWord(ctx, reqData)
 		assert.Error(t, err)
 		assert.Equal(t, word_service.ErrUnauthorized, err)
 
 	})
 	t.Run("DuplicateWord_Errors", func(t *testing.T) {
 		// 3. 既存の単語がある場合
-		_, err = client.Word.Create().SetName("duplicate").Save(ctx)
-		assert.NoError(t, err)
+		// _, err = client.Word.Create().SetName("duplicate").Save(ctx)
+		// assert.NoError(t, err)
 
-		reqData = &models.CreateWordRequest{
-			Name:   "duplicate", // 同じ名前の単語
+		reqData = &models.UpdateWordRequest{
+			ID:     word2.ID,
+			Name:   "banana", // 同じ名前の単語
 			UserID: adminUser.ID,
 			WordInfos: []models.WordInfo{
-				{PartOfSpeechID: 1, JapaneseMeans: []models.JapaneseMean{{Name: "ダブリ"}}},
+				{ID: 2, PartOfSpeechID: 3, JapaneseMeans: []models.JapaneseMean{{ID: 2, Name: "テスト"}}},
+				{ID: 3, PartOfSpeechID: 4, JapaneseMeans: []models.JapaneseMean{{ID: 3, Name: "テスト2"}}},
 			},
 		}
 
-		_, err = wordService.CreateWord(ctx, reqData)
+		_, err = wordService.UpdateWord(ctx, reqData)
 		assert.Error(t, err)
-		assert.Equal(t, word_service.ErrWordExists, err)
+		assert.Equal(t, "failed to query word", err.Error())
 
 	})
-	t.Run("CreateWordInfo_Errors", func(t *testing.T) {
+	t.Run("UpdateWordInfo_Errors", func(t *testing.T) {
 		// 4. WordInfo作成エラー
-		reqData = &models.CreateWordRequest{
+		reqData = &models.UpdateWordRequest{
 			Name:   "newword",
 			UserID: adminUser.ID,
 			WordInfos: []models.WordInfo{
@@ -117,14 +182,14 @@ func TestCreateWord(t *testing.T) {
 			},
 		}
 
-		_, err = wordService.CreateWord(ctx, reqData)
+		_, err = wordService.UpdateWord(ctx, reqData)
 		assert.Error(t, err)
-		assert.Equal(t, word_service.ErrCreateWordInfo, err) // 比較を修正
+		assert.Contains(t, err.Error(), "failed to create word info")
 
 	})
 	t.Run("CreateJapaneseMean_Errors", func(t *testing.T) {
 		// 5. JapaneseMean作成エラー
-		reqData = &models.CreateWordRequest{
+		reqData = &models.UpdateWordRequest{
 			Name:   "validword",
 			UserID: adminUser.ID,
 			WordInfos: []models.WordInfo{
@@ -137,8 +202,8 @@ func TestCreateWord(t *testing.T) {
 			},
 		}
 
-		_, err = wordService.CreateWord(ctx, reqData)
+		_, err = wordService.UpdateWord(ctx, reqData)
 		assert.Error(t, err)
-		assert.Equal(t, word_service.ErrCreateJapaneseMean, err)
+		assert.Contains(t, err.Error(), "failed to create japanese mean")
 	})
 }

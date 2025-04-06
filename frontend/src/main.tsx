6ca8cebf9ee8849fe39d1b1ae0753ea12df46728
kousuke-enoki <kokoeno129@gmail.com>
2025-04-06 18:10:05 +0900

    08_02 setting light,dark mode 実装

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,17 +5,21 @@ import './index.css'
 import App from './App'
 // import reportWebVitals from './reportWebVitals'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// ダークモード等の切り替え用
+import { ThemeProvider } from './context/ThemeContext'
 
 // const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 const queryClient = new QueryClient()
 // root.render(
 
 createRoot(document.getElementById('root')!).render(
+
   <StrictMode>
-    {/* <QueryClientProvider client={queryClient}> */}
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+      </ThemeProvider>
     </StrictMode>,
 )
 

6f1009fac6f606b49313862ce3202dfe1d664b01
kousuke-enoki <kokoeno129@gmail.com>
2025-07-14 18:51:46 +0900

    13-01 infra db設定など

--- infra/bin/myapp.ts
+++ infra/bin/myapp.ts
@@ -4,11 +4,27 @@ import { App } from 'aws-cdk-lib';
 import { NetworkStack } from '../lib/network-stack';
 import { DbStack } from '../lib/db-stack';
 import { AppStack } from '../lib/app-stack';
+import { OpsStack } from '../lib/ops-stack';
 
 const app  = new App();
 const env  = { account: process.env.CDK_DEFAULT_ACCOUNT,
                region : process.env.CDK_DEFAULT_REGION };
 
-const net  = new NetworkStack(app, 'NetStack', { env });
-const db   = new DbStack(app,  'DbStack',  { env, vpc: net.vpc });
-new AppStack(app,'AppStack',  { env, vpc: net.vpc, secret: db.secret });
+const net = new NetworkStack(app,'NetStack',{ env });
+const db  = new DbStack(app,'DbStack',{ env, vpc: net.vpc });
+// AppStack は VPC と DB を参照する
+// そのため、net と db の後に定義する必要がある
+new AppStack(app,'AppStack',{
+  env,
+  vpc: net.vpc,
+  secret: db.secret,
+  db: db.db,
+  lambdaSg: db.lambdaToDbSecurityGroup,
+});
+
+new OpsStack(app,'OpsStack',{
+  env,
+  db: db.db,               // ← RDS だけ渡す
+  stopHourJst: 0,          // 好みで変更可
+  startHourJst: 9,
+});
--- infra/lib/network-stack.ts
+++ infra/lib/network-stack.ts
@@ -3,14 +3,24 @@ import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export interface NetworkStackProps extends StackProps {}
-
+/**
+ * ネットワークスタック
+ * VPC を定義し、他のスタックから参照できるようにする
+ */
 export class NetworkStack extends Stack {
-  /** 別 Stack から参照できるよう公開 */
+  /** 他スタックから参照するため公開 */
   public readonly vpc: ec2.Vpc;
 
-  constructor(scope: Construct, id: string, props?: NetworkStackProps) {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    this.vpc = new ec2.Vpc(this, 'Vpc', { maxAzs: 2 });
+    this.vpc = new ec2.Vpc(this, 'Vpc', {
+      maxAzs: 2,
+      natGateways: 0,           // NAT コスト 0 円
+      subnetConfiguration: [
+        { name: 'Public',   subnetType: ec2.SubnetType.PUBLIC,           cidrMask: 24 },
+        { name: 'Isolated', subnetType: ec2.SubnetType.PRIVATE_ISOLATED, cidrMask: 24 },
+      ],
+    });
   }
 }

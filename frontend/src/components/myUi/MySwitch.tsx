a0f8f0faab39811153808142a127883adc45408b
kousuke-enoki <kokoeno129@gmail.com>
2025-09-04 15:22:41 +0900

    14-01 デザイン修正 bulkregister, quiz

--- frontend/src/components/myUi/MySwitch.tsx
+++ frontend/src/components/myUi/MySwitch.tsx
@@ -1,12 +1,11 @@
-import { Switch } from '@headlessui/react';
-import clsx from 'clsx';
+import { Switch } from '@headlessui/react'
+import clsx from 'clsx'
 
 type Props = {
-  checked: boolean;
-  onChange: (v: boolean) => void;
-  id: string;
-};
-
+  checked: boolean
+  onChange: (v: boolean) => void
+  id: string
+}
 
 export const MySwitch = ({ checked, onChange, id }: Props) => (
   <Switch
@@ -15,14 +14,14 @@ export const MySwitch = ({ checked, onChange, id }: Props) => (
     onChange={onChange}
     className={clsx(
       'appearance-none p-0 border-0 relative inline-flex h-6 w-11 items-center rounded-full transition-colors',
-      checked ? 'bg-blue-600' : 'bg-gray-300'
+      checked ? 'bg-blue-600' : 'bg-gray-300',
     )}
   >
     <span
       className={clsx(
         'inline-block h-5 w-5 transform rounded-full bg-white shadow transition',
-        checked ? 'translate-x-5' : 'translate-x-1'
+        checked ? 'translate-x-5' : 'translate-x-1',
       )}
     />
   </Switch>
-);
+)
